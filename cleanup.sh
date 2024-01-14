867a3b81fb3535684b3310ef365fe0ecb81d5ccd
MoonFruit <dkmoonfruit@gmail.com>
2024-01-14 11:22:08 +0800

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 bfg -D "*.{commit,json,srs,db}"
+FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch --prune-empty --force
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
