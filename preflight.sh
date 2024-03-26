a1deecb8a7191d7f0cce83608383e5bdfee06802
MoonFruit <dkmoonfruit@gmail.com>
2024-03-26 14:53:34 +0800

    bug fix

--- preflight.sh
+++ preflight.sh
@@ -5,8 +5,8 @@ V2RAY_RULES_COMMIT=preflight/v2ray-rules-dat.commit
 CLASS_CONFIG_SHA1=preflight/clash-config.sha1
 
 check-v2ray-rules() {
-    echo -n "Loyalsoldier/v2ray-rules-dat: "
-    git -C dat rev-parse HEAD | tee "$TEMP"
+    echo -n "Loyalsoldier/v2ray-rules-dat: " >&2
+    git -C dat rev-parse HEAD | tee "$TEMP" >&2
     if diff "$TEMP" "$V2RAY_RULES_COMMIT"; then
         echo "Loyalsoldier/v2ray-rules-dat is not changed" >&2
         return 1
@@ -17,8 +17,8 @@ check-v2ray-rules() {
 }
 
 check-clash-url() {
-    echo -n "clash-config: "
-    sha1sum dat/clash-config.yaml | awk '{print $1}' | tee "$TEMP"
+    echo -n "clash-config: " >&2
+    sha1sum dat/clash-config.yaml | awk '{print $1}' | tee "$TEMP" >&2
     if diff "$TEMP" "$CLASS_CONFIG_SHA1"; then
         echo "clash-config is not changed" >&2
         return 1
@@ -28,4 +28,7 @@ check-clash-url() {
     fi
 }
 
-check-v2ray-rules || check-clash-url
+RESULT=1
+check-v2ray-rules && RESULT=0
+check-clash-url && RESULT=0
+exit $RESULT
