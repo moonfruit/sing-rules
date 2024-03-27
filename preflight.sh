1d8191778f53ca8619ee77f1785be6c71cee1cfa
MoonFruit <dkmoonfruit@gmail.com>
2024-03-27 10:52:14 +0800

    bug fix

--- preflight.sh
+++ preflight.sh
@@ -8,30 +8,34 @@ TEMP=$(mktemp)
 mkdir -p "$DIR"
 
 check-v2ray-rules() {
-    echo -n "Loyalsoldier/v2ray-rules-dat: " >&2
-    git -C dat rev-parse HEAD | tee "$TEMP" >&2
+    echo -n "Loyalsoldier/v2ray-rules-dat: "
+    git -C dat rev-parse HEAD | tee "$TEMP"
     if diff "$TEMP" "$V2RAY_RULES_COMMIT"; then
-        echo "Loyalsoldier/v2ray-rules-dat is not changed" >&2
+        echo "Loyalsoldier/v2ray-rules-dat is not changed"
         return 1
     else
-        echo "BUILD_RULES=1"
         mv "$TEMP" "$V2RAY_RULES_COMMIT"
     fi
 }
 
 check-clash-url() {
-    echo -n "clash-config: " >&2
-    sha1sum dat/clash-config.yaml | awk '{print $1}' | tee "$TEMP" >&2
+    echo -n "clash-config: "
+    sha1sum dat/clash-config.yaml | awk '{print $1}' | tee "$TEMP"
     if diff "$TEMP" "$CLASS_CONFIG_SHA1"; then
-        echo "clash-config is not changed" >&2
+        echo "clash-config is not changed"
         return 1
     else
-        echo "BUILD_CONFIG=1"
         mv "$TEMP" "$CLASS_CONFIG_SHA1"
     fi
 }
 
 RESULT=1
-check-v2ray-rules && RESULT=0
-check-clash-url && RESULT=0
+if check-v2ray-rules >&2; then
+    echo "BUILD_RULES=1"
+    RESULT=0
+fi
+if check-clash-url >&2; then
+    echo "BUILD_CONFIG=1"
+    RESULT=0
+fi
 exit $RESULT
