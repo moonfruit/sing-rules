838fa645fcdcc1a02ff8e7a04ec4718c4d839e53
MoonFruit <dkmoonfruit@gmail.com>
2024-04-02 14:45:22 +0800

    bug fix

--- npm-publish.sh
+++ npm-publish.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 ]]; then
-    echo "No version"
+VERSION=$1
+if [[ -z $VERSION ]]; then
+    echo "No version defined"
     exit 1
+fi
+
+if [[ $VERSION =~ ([0-9]{8})([0-9]{4}) ]]; then
+    VERSION=1.${BASH_REMATCH[1]}.${BASH_REMATCH[2]#0}
+else
+    VERSION=1.$VERSION.0
 fi
 
 cat >package.json <<END
 {
   "name": "sing-rules",
-  "version": "1.$1",
+  "version": "$VERSION",
   "description": "sing-box rule sets base on Loyalsoldier/v2ray-rules-dat",
+  "license": "GPL-3.0-or-later",
   "homepage": "https://github.com/moonfruit/sing-rules#readme",
   "repository": {
     "type": "git",
     "url": "git+https://github.com/moonfruit/sing-rules.git"
   },
-  "license": "GPL-3.0-or-later",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
   "files": [
     "rules/*"
   ]
 }
 END
 
-npm publish
+npm publish --dry-run
