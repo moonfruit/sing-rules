ceb048c6468037170627a5a27bc3bb767ddb981d
MoonFruit <dkmoonfruit@gmail.com>
2024-09-06 17:42:14 +0800

    bug fix

--- build-sing-config.sh
+++ build-sing-config.sh
@@ -2,18 +2,16 @@
 set -e
 BIN=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
-if (($# < 3)); then
-    echo "$0: user token config" >&2
+if (($# < 2)); then
+    echo "$0: user token" >&2
     exit 1
 fi
-
-CONFIG=$(realpath "$3")
 
 cd "$BIN"
 rm -fr private
 git clone "https://$1:$2@gitee.com/moonfruit/private.git"
 
-cd private
-"$BIN/clash-to-sing.py" -c "$CONFIG" | sing-box format -c /dev/stdin >config.json
+"$BIN/clash-to-sing.py" -c config/config.json | sing-box format -c /dev/stdin >private/config.json
 
+cd private
 "$BIN/commit-and-push.sh" "Update config" || true
