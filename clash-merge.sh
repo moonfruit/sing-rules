7b0f14448b79112206c6d6059556bb299b838ea4
MoonFruit <dkmoonfruit@gmail.com>
2025-02-27 11:30:16 +0800

    bug fix

--- clash-merge.sh
+++ clash-merge.sh
@@ -9,6 +9,7 @@ for LIST in "$1"/*.list; do
     if [[ -r "$1/$BASENAME.exclude" ]]; then
         echo "Excluding $BASENAME.json from $1/$BASENAME.exclude"
         grep -f "$1/$BASENAME.exclude" "$BASENAME.json"
-        grep -vf "$1/$BASENAME.exclude" "$BASENAME.json" | sponge "$BASENAME.json"
+        grep -vf "$1/$BASENAME.exclude" "$BASENAME.json" >"$BASENAME.json.new"
+        mv "$BASENAME.json.new" "$BASENAME.json"
     fi
 done
