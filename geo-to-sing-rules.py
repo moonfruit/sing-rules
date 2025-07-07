5de949d7f6ebb41da4f1c0506416f2850fac1f32
MoonFruit <dkmoonfruit@gmail.com>
2025-07-07 13:43:18 +0800

    Update rules

--- geo-to-sing-rules.py
+++ geo-to-sing-rules.py
@@ -55,7 +55,7 @@ def generate(filename: str, *, geosite: RelaxedStrings | None = None, geoip: Rel
 
 
 def main():
-    generate("ai.json", geosite=["anthropic", "bing", "jetbrains-ai", "openai", "perplexity"])
+    generate("ai.json", geosite=["anthropic", "jetbrains-ai", "openai", "perplexity"])
     generate("netflix.json", geosite="netflix", geoip="netflix")
     generate("tiktok.json", geosite="bytedance@!cn")
     generate("youtube.json", geosite="youtube")
